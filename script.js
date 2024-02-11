0f1b22c6ea6a104577aeaa9273e0b2cba6c20701
Manshi-12 <159686169+Manshi-12@users.noreply.github.com>
2024-02-11 15:11:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 
-function fun() 
-{
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  for (var i = 0; i < names.length; i++) 
-  {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter == 'j') 
-    {
-      byeSpeaker(names[i]);
-    }
-    else 
-    {
-      helloSpeaker(names[i]);
-    }
-  }    
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
 }
+
+})();
+
